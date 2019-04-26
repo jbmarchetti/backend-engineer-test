e15c9213d9a2f2fedd4fd2159785b504b01b8723
Jean Baptiste Marchetti <marchetti.jb@gmail.com>
2019-04-26 09:17:50 +0200

    test(freelancerService): Test on more periods - merging duplicate code into a function

--- src/freelancer/freelancer.service.spec.js
+++ src/freelancer/freelancer.service.spec.js
@@ -4,6 +4,25 @@ const fs = require('fs');
 const FreelancerService = require('./freelancer.service');
 const periods = require('../../test/datasets/periods/periods.js');
 
+
+/**
+ *
+ *
+ * @param {json} fJson
+ * @param {boolean} [emptySkills=false]
+ */
+function checkJsonFreelancer(fJson, emptySkills=false) {
+  const freelancer = new FreelancerService(fJson).getFreeLancerWithComputedSkills();
+  expect(freelancer).to.be.an('object');
+  expect(freelancer).to.have.property('freelance');
+  expect(freelancer.freelance).to.have.property('id');
+  if (emptySkills) {
+    expect(freelancer.freelance).to.have.property('computedSkills').and.to.be.an('array').to.be.empty;
+  } else {
+    expect(freelancer.freelance).to.have.property('computedSkills').and.to.be.an('array');
+  }
+}
+
 describe('Freelance Service', () => {
   before(() => {
     this.emptyFreelancerJson =fs.readFileSync('test/datasets/freelancers/empty-freelancer.json', 'utf8');
@@ -18,26 +37,12 @@ describe('Freelance Service', () => {
   });
 
 
-  describe('Compute Skill', () => {
-    it('should return 11 - array sorted by startDate', () => {
-      const months = new FreelancerService({}).computeSkill(periods.periods11Months);
-      expect(months).to.equal(11);
-    });
-
-    it('should return 11 - array unsorted', () => {
-      const months = new FreelancerService({}).computeSkill(periods.periods11MonthsBis);
-      expect(months).to.equal(11);
-    });
-
-    it('should return 0 if array is empty', () => {
-      const months = new FreelancerService({}).computeSkill(periods.periods0Months);
-      expect(months).to.equal(0);
-    });
-
-
-    it('should return 60', () => {
-      const months = new FreelancerService({}).computeSkill(periods.periods60Months);
-      expect(months).to.equal(60);
+  describe('Compute Skill - Sum of months by periods', () => {
+    periods.datas.forEach((p) => {
+      it('should return '+p.result, () => {
+        const months = new FreelancerService({}).computeSkill(p.periods);
+        expect(months).to.equal(p.result);
+      });
     });
   });
 
@@ -59,37 +64,21 @@ describe('Freelance Service', () => {
     });
   });
 
-  describe('Get FreeLancer With Computed Skills', () => {
-    it('should return a freelancer with his computed skills', () => {
-      const freelancer = new FreelancerService(this.fullFreelancerJson).getFreeLancerWithComputedSkills();
-      expect(freelancer).to.be.an('object');
-      expect(freelancer).to.have.property('freelance');
-      expect(freelancer.freelance).to.have.property('id');
-      expect(freelancer.freelance).to.have.property('computedSkills').and.to.be.an('array');
+  describe('Get FreeLancer with the total number of months he has worked', () => {
+    it('should return freelancer with his skills', () => {
+      checkJsonFreelancer(this.fullFreelancerJson);
     });
 
-    it('should return freelancer with an empty array', () => {
-      const freelancer = new FreelancerService(this.emptyFreelancerJson).getFreeLancerWithComputedSkills();
-      expect(freelancer).to.be.an('object');
-      expect(freelancer).to.have.property('freelance');
-      expect(freelancer.freelance).to.have.property('id');
-      expect(freelancer.freelance).to.have.property('computedSkills').and.to.be.an('array').to.be.empty;
+    it('should return freelancer with an empty skills array', () => {
+      checkJsonFreelancer(this.emptyFreelancerJson, true);
     });
 
-    it('should return freelancer with an empty array when no experiences', () => {
-      const freelancer = new FreelancerService(this.noExpFreelancerJson).getFreeLancerWithComputedSkills();
-      expect(freelancer).to.be.an('object');
-      expect(freelancer).to.have.property('freelance');
-      expect(freelancer.freelance).to.have.property('id');
-      expect(freelancer.freelance).to.have.property('computedSkills').and.to.be.an('array').to.be.empty;
+    it('should return freelancer with an empty skills array when no experiences', () => {
+      checkJsonFreelancer(this.noExpFreelancerJson, true);
     });
 
-    it('should return freelancer with an empty array when no skills', () => {
-      const freelancer = new FreelancerService(this.noSkillsFreelancerJson).getFreeLancerWithComputedSkills();
-      expect(freelancer).to.be.an('object');
-      expect(freelancer).to.have.property('freelance');
-      expect(freelancer.freelance).to.have.property('id');
-      expect(freelancer.freelance).to.have.property('computedSkills').and.to.be.an('array').to.be.empty;
+    it('should return freelancer with an empty skills array when no skills', () => {
+      checkJsonFreelancer(this.noSkillsFreelancerJson, true);
     });
   });
 });
--- test/datasets/periods/periods.js
+++ test/datasets/periods/periods.js
@@ -1,39 +1,86 @@
 const moment = require('moment');
-exports.periods11Months=
-   [
-     {
-       startDate: moment('2017-01-01T00:00:00+01:00'),
-       endDate: moment('2017-03-01T00:00:00+01:00'),
-     },
-     {
-       startDate: moment('2018-01-01T00:00:00+01:00'),
-       endDate: moment('2018-05-01T00:00:00+01:00'),
-     },
-     {
-       startDate: moment('2018-03-01T00:00:00+01:00'),
-       endDate: moment('2018-10-01T00:00:00+01:00'),
-     },
-   ];
-
-exports.periods11MonthsBis =
+exports.datas =
+  [
+    {
+      periods: [
+        {
+          startDate: moment('2017-01-01T00:00:00+01:00'),
+          endDate: moment('2017-12-01T00:00:00+01:00'),
+        },
+      ],
+      result: 11,
+    },
+    {
+      periods:
     [
       {
-        startDate: moment('2018-03-01T00:00:00+01:00'),
-        endDate: moment('2018-10-01T00:00:00+01:00'),
-      }, {
         startDate: moment('2017-01-01T00:00:00+01:00'),
         endDate: moment('2017-03-01T00:00:00+01:00'),
       },
       {
-        startDate: moment('2018-01-01T00:00:00+01:00'),
-        endDate: moment('2018-05-01T00:00:00+01:00'),
+        startDate: moment('2017-01-01T00:00:00+01:00'),
+        endDate: moment('2017-12-01T00:00:00+01:00'),
       },
+    ],
+      result: 11,
+    },
+    {
+      periods:
+        [
+          {
+            startDate: moment('2016-01-01T00:00:00+01:00'),
+            endDate: moment('2016-12-01T00:00:00+01:00'),
+          },
+          {
+            startDate: moment('2017-01-01T00:00:00+01:00'),
+            endDate: moment('2017-03-01T00:00:00+01:00'),
+          },
+          {
+            startDate: moment('2017-01-01T00:00:00+01:00'),
+            endDate: moment('2017-12-01T00:00:00+01:00'),
+          },
+        ],
+      result: 22,
+    },
+    {
+      periods:
+      [
+        {
+          startDate: moment('2016-01-01T00:00:00+01:00'),
+          endDate: moment('2016-12-01T00:00:00+01:00'),
+        },
+        {
+          startDate: moment('2018-01-01T00:00:00+01:00'),
+          endDate: moment('2018-12-01T00:00:00+01:00'),
+        },
+        {
+          startDate: moment('2017-01-01T00:00:00+01:00'),
+          endDate: moment('2017-12-01T00:00:00+01:00'),
+        },
+      ],
+      result: 33,
+    },
+    {
+      periods:
+      [
+        {
+          startDate: moment('2016-01-01T00:00:00+01:00'),
+          endDate: moment('2017-12-01T00:00:00+01:00'),
+        },
+        {
+          startDate: moment('2017-01-01T00:00:00+01:00'),
+          endDate: moment('2017-12-01T00:00:00+01:00'),
+        },
+      ],
+      result: 23,
+    },
+    {
 
-    ];
+      periods: [], result: 0,
+    },
+    {
 
-exports.periods0Months = [];
-
-exports.periods60Months =
+      periods:
     [
       {
         startDate: moment('2016-03-01T00:00:00+01:00'),
@@ -47,4 +94,6 @@ exports.periods60Months =
         startDate: moment('2013-05-01T00:00:00+01:00'),
         endDate: moment('2014-07-01T00:00:00+01:00'),
       },
-    ];
+    ], result: 60,
+    },
+  ];
